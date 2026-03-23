df3e3cc96af741cbbf374dc5c90e9f5b2021c8d4
Matthias Langer <128157644+vicecard@users.noreply.github.com>
2026-03-23 14:03:36 +0100

    fixed naming of script file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from machine_learning.object_detection.object_detector import ObjectDetector
+from data_io import download_dataset
+
+
 def main():
     print("Hello from cargo-detection!")
+    print(ObjectDetector.__dict__)
+    print(download_dataset.run)
 
 
 if __name__ == "__main__":
