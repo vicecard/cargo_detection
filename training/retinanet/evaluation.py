fe3ea99d3e2081ce46750ccf322e061c93d9a138
Trainer on Container <trainer@container.local>
2026-04-22 05:29:22 +0000

    new model trained

--- training/retinanet/evaluation.py
+++ training/retinanet/evaluation.py
@@ -2,6 +2,7 @@ import multiprocessing
 from pathlib import Path
 
 import torch
+from torch.utils.data import ConcatDataset
 
 import torchvision.transforms.v2 as T
 from torchvision.datasets.coco import CocoDetection
@@ -32,9 +33,19 @@ def main(model_path: Path, dataset_name: str, output_path: Path) -> None:
             T.SanitizeBoundingBoxes(),
         ]
     )
-    dataset: CocoDetection = CocoDetection(root=images_root / "images" / "default",
-                                           annFile=str(dataset_root / "annotations" / "instances_default.json"),
-                                           transforms=transform_fns)
+    #dataset: CocoDetection = CocoDetection(root=images_root / "images" / "default",
+    #                                       annFile=str(dataset_root / "annotations" / "instances_default.json"),
+    #                                       transforms=transform_fns)
+
+    dataset_01: CocoDetection = CocoDetection(root=images_root / "images" / "ds1",
+                                              annFile=str(dataset_root / "annotations" / "instances_ds1.json"),
+                                              transforms=transform_fns)
+
+    dataset_02: CocoDetection = CocoDetection(root=images_root / "images" / "ds2",
+                                              annFile=str(dataset_root / "annotations" / "instances_ds2.json"),
+                                              transforms=transform_fns)
+
+    dataset: ConcatDataset = ConcatDataset([dataset_01, dataset_02])
 
     datasets = utils.partition_dataset(dataset, lengths=(0.7, 0.2, 0.1))
 
@@ -55,7 +66,7 @@ def main(model_path: Path, dataset_name: str, output_path: Path) -> None:
                          detector,
                          output_path,
                          (0, 0),
-                         16,
+                         32,
                          conf_thres=0.10,
                          iou_thres=0.05,
                          device=device,
@@ -65,9 +76,9 @@ def main(model_path: Path, dataset_name: str, output_path: Path) -> None:
 if __name__ == "__main__":
     multiprocessing.set_start_method(method="spawn")
 
-    output_path: Path = Path().absolute() / "evaluation_images" / "version_02"
-    main(model_path="retinanet_cargo_shipping_labels/version_1/checkpoints/epoch=26-step=12582.ckpt",
-         dataset_name="data/datasets/coco/merged",
+    output_path: Path = Path().absolute() / "evaluation_images" / "version_03"
+    main(model_path=Path("retinanet_cargo_shipping_labels/version_2/checkpoints/epoch=23-step=24360.ckpt"),
+         dataset_name="data/datasets/coco/merged_variant",
          output_path=output_path)
 
     print("Done!")
@@ -81,3 +92,7 @@ if __name__ == "__main__":
     # lightning_logs/retinanet_cargo_shipping_labels/version_1/checkpoints/epoch=26-step=12582.ckpt
     # 0.3779895603656769
     # {'map': tensor(0.4250), 'map_50': tensor(0.5598), 'map_75': tensor(0.4488), 'map_small': tensor(0.1510), 'map_medium': tensor(0.2038), 'map_large': tensor(0.5157), 'mar_1': tensor(0.2682), 'mar_10': tensor(0.4436), 'mar_100': tensor(0.4989), 'mar_small': tensor(0.1884), 'mar_medium': tensor(0.2306), 'mar_large': tensor(0.5866), 'map_per_class': tensor(-1.), 'mar_100_per_class': tensor(-1.), 'classes': tensor([1, 2], dtype=torch.int32)}
+    # 3)
+    # lightning_logs/retinanet_cargo_shipping_labels/version_2/checkpoints/epoch=23-step=24360.ckpt
+    # 0.5916559100151062
+    # {'map': tensor(0.6503), 'map_50': tensor(0.8531), 'map_75': tensor(0.7197), 'map_small': tensor(0.1913), 'map_medium': tensor(0.5523), 'map_large': tensor(0.7504), 'mar_1': tensor(0.2727), 'mar_10': tensor(0.6585), 'mar_100': tensor(0.7388), 'mar_small': tensor(0.2495), 'mar_medium': tensor(0.7226), 'mar_large': tensor(0.8471), 'map_per_class': tensor(-1.), 'mar_100_per_class': tensor(-1.), 'classes': tensor([1, 2], dtype=torch.int32)}
